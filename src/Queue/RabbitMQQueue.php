eb8bf7f9fbf21599fb0c59806140ceebb021827c
luffyzhao <luffyzhao@vip.126.com>
2022-12-20 12:00:35 +0800

    添加rbm

---
+++ src/Queue/RabbitMQQueue.php
@@ -0,0 +1,28 @@
+<?php
+namespace Job\Queue;
+
+use Interop\Amqp\AmqpQueue;
+use ladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue as RabbitMQQueueBase;
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob;
+
+class RabbitMQQueue extends RabbitMQQueueBase
+{
+    /** {@inheritdoc} */
+    public function popForTimeout($queueName = null, $timeout = 0)
+    {
+        try {
+            /** @var AmqpQueue $queue */
+            [$queue] = $this->declareEverything($queueName);
+
+            $consumer = $this->context->createConsumer($queue);
+
+            if ($message = $consumer->receive($timeout)) {
+                return new RabbitMQJob($this->container, $this, $consumer, $message);
+            }
+        } catch (\Throwable $exception) {
+            $this->reportConnectionError('pop', $exception);
+
+            return;
+        }
+    }
+}
